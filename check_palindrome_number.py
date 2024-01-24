68f967144fe02967b809bee622178c16e923e9ac
Your Name <you@example.com>
2024-01-24 17:50:54 +0800

    check_palindrome_number

--- check_palindrome_number.py
+++ check_palindrome_number.py
@@ -1,3 +1,28 @@
 # Write a program to check if the given number is a palindrome number.
 
 # A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
+
+
+def is_palindrome(number):
+     # Convert the number to a string for easier comparison
+    str_number = str(number)
+
+    # Check if the string representation is the same when reversed
+    return str_number == str_number[::-1]
+
+# Given example:
+number1 = 121
+number2 = 125
+
+# Check and the display results
+print(f"original number {number1}")
+if is_palindrome(number1):
+    print("Yes. given number is a palindrome number")
+else:
+    print("No. given number is not a palindrome number")
+
+print(f"\noriginal number {number2}")
+if is_palindrome(number2):
+    print("Yes. given number is a palindrome number")
+else:
+    print("No. given number is not a palindrome number")
