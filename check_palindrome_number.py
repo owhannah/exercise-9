bfeb4d5491671a8dddbd7b616dd1c2176ef1f94b
Your Name <you@example.com>
2024-01-24 17:03:18 +0800

    Write a program to check if the given number is a palindrome number

---
+++ check_palindrome_number.py
@@ -0,0 +1,3 @@
+# Write a program to check if the given number is a palindrome number.
+
+# A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
